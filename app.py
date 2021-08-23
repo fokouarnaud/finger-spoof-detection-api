415a1687b0d2139e2f2f1d435fd786cc7c2e7731
fokouarnaud <fokouarnaud@gmail.com>
2021-08-23 12:53:13 +0100

    add iris type predict

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 from base import Movies, db
 from flask import Flask
 from flask_restful import Resource, reqparse, Api  # Instantiate a flask object
+import numpy as np
+import pickle
 
 app = Flask(__name__)
 
@@ -23,6 +25,10 @@ app.app_context().push()
 
 # Create the databases
 db.create_all()  # Creating a class to create get, post, put & delete methods
+
+
+# load the pickle model
+model = pickle.load(open("model.pkl", "rb"))
 
 
 class Movies_List(Resource):
@@ -82,9 +88,39 @@ class All_Movies(Resource):  # Defining the get method
         # Adding the URIs to the api
         return {'Movies': list(map(lambda x: x.json(), Movies.query.all()))}
 
+# Creating a class to get iris prediction
+
+
+class IrisPredict(Resource):  # Defining the get method
+    # Instantiating a parser object to hold data from message payload
+    parser = reqparse.RequestParser()
+    parser.add_argument('sepal_length', type=str, required=True,
+                        help='sepal_length')
+    parser.add_argument('sepal_width', type=str, required=True,
+                        help='sepal_width')
+    parser.add_argument('petal_length', type=str, required=True,
+                        help='petal_length')
+    parser.add_argument('petal_width', type=str, required=True,
+                        help='petal_width')
+
+    def get(self):
+        args = IrisPredict.parser.parse_args()
+        float_features = [
+            float(args.get("sepal_length")),
+            float(args.get("sepal_width")),
+            float(args.get("petal_length")),
+            float(args.get("petal_width"))
+        ]
+
+        print(float_features)
+        features = [np.array(float_features)]
+        prediction = model.predict(features)
+        return {'prediction_text':  "The flower species is {}".format(prediction)}
+
 
 api.add_resource(All_Movies, '/')
 api.add_resource(Movies_List, '/<string:movie>')
+api.add_resource(IrisPredict, '/iris')
 
 if __name__ == '__main__':
     # Run the applications
