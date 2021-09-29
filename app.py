a17cc956dbb05266a32b9009d6f9a822b7a798dd
fokouarnaud <fokouarnaud@gmail.com>
2021-09-29 19:23:37 +0100

    update

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 # app.py#Import necessary packages
-from base import Movies, db
+from base import Candidate,Classroomsubjectclass,classroomsubjectclasscandidate, db
 from flask import Flask
 from flask_restful import Resource, reqparse, Api  # Instantiate a flask object
-import numpy as np
-import pickle
+#import numpy as np
+#import pickle
 
 app = Flask(__name__)
 
@@ -11,7 +11,7 @@ app = Flask(__name__)
 api = Api(app)
 
 # Setting the location for the sqlite database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///base.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test_one.db'
 
 # Adding the configurations for the database
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -28,99 +28,137 @@ db.create_all()  # Creating a class to create get, post, put & delete methods
 
 
 # load the pickle model
-model = pickle.load(open("model.pkl", "rb"))
+#model = pickle.load(open("model.pkl", "rb"))
 
-
-class Movies_List(Resource):
+class Candidate_List(Resource):
 
     # Instantiating a parser object to hold data from message payload
     parser = reqparse.RequestParser()
-    parser.add_argument('director', type=str, required=False,
-                        help='Director of the movie')
-    parser.add_argument('genre', type=str, required=False,
-                        help='Genre of the movie')
-    parser.add_argument('collection', type=int, required=True,
-                        help='Gross collection of the movie')
+    parser.add_argument('keypoints', type=str, required=False,
+                        help='keypoints of the candidate')
+    parser.add_argument('descriptors', type=str, required=False, help='descriptions of the candidate')
+    
 
     # Creating the get method
-    def get(self, movie):
-        item = Movies.find_by_title(movie)
+    def get(self, name):
+        item = Candidate.find_by_name(name)
         if item:
             return item.json()
-        return {'Message': 'Movie is not found'}
+        return {'Message': 'Candidate is not found'}
 
     # Creating the post method
-    def post(self, movie):
-        if Movies.find_by_title(movie):
-            return {' Message': 'Movie with the  title {} already exists'.format(movie)}
+    def post(self, name):
+        if Candidate.find_by_name(name):
+            return {' Message': 'Candidate with the  name {} already exists'.format(name)}
 
-        args = Movies_List.parser.parse_args()
-        item = Movies(movie, args['director'],
-                      args['genre'],      args['collection'])
+        args = Candidate_List.parser.parse_args()
+        item = Candidate(name,args['keypoints'],
+                      args['descriptors'])
 
         item.save_to()
         return item.json()
 
     # Creating the put method
-    def put(self, movie):
-        args = Movies_List.parser.parse_args()
-        item = Movies.find_by_title(movie)
+    def put(self, name):
+        args = Candidate_List.parser.parse_args()
+        item = Candidate.find_by_name(name)
         if item:
-            item.collection = args['collection']
+            item.keypoints = args['keypoints']
+            item.descriptors = args['descriptors']
             item.save_to()
-            return {'Movie': item.json()}
-        item = Movies(movie, args['director'],
-                      args['genre'], args['collection'])
+            return {'Candidate': item.json()}
+        item = Candidate(name, args['keypoints'],
+                      args['descriptors'])
         item.save_to()
         return item.json()  # Creating the delete method
 
-    def delete(self, movie):
-        item = Movies.find_by_title(movie)
+    def delete(self, name):
+        item = Candidate.find_by_name(name)
         if item:
             item.delete_()
-            return {'Message': '{} has been deleted from records'.format(movie)}
-        return {'Message': '{} is already not on the list'.format()}
+            return {'Message': '{} has been deleted from records'.format(name)}
+        return {'Message': '{} is already not on the list'.format(name)}
 
 
 # Creating a class to get all the movies from the database.
-class All_Movies(Resource):  # Defining the get method
+class All_Candidate(Resource):  # Defining the get method
     def get(self):
         # Adding the URIs to the api
-        return {'Movies': list(map(lambda x: x.json(), Movies.query.all()))}
+        return {'Candidates': list(map(lambda x: x.json(), Candidate.query.all()))}
+
+
+class Classroomsubjectclass_List(Resource):
+
+    # Creating the get method
+    def get(self, name):
+        item = Classroomsubjectclass.find_by_name(name)
+        if item:
+            return item.json()
+        return {'Message': 'Classroom subject class is not found'}
+
+    # Creating the post method
+    def post(self, name):
+        if Classroomsubjectclass.find_by_name(name):
+            return {' Message': 'Classroom subject class with the  name {} already exists'.format(name)}
+
+       
+        item = Classroomsubjectclass(name)
+
+        item.save_to()
+        return item.json()
+
+
+    def delete(self, name):
+        item = Classroomsubjectclass.find_by_name(name)
+        if item:
+            item.delete_()
+            return {'Message': '{} has been deleted from records'.format(name)}
+        return {'Message': '{} is already not on the list'.format(name)}
+
+
+# Creating a class to get all the movies from the database.
+class All_Classroomsubjectclass(Resource):  # Defining the get method
+    def get(self):
+        # Adding the URIs to the api
+        return {'Classroomsubjectclass': list(map(lambda x: x.json(), Classroomsubjectclass.query.all()))}
+
 
 # Creating a class to get iris prediction
 
 
-class IrisPredict(Resource):  # Defining the get method
+#class IrisPredict(Resource):  # Defining the get method
     # Instantiating a parser object to hold data from message payload
-    parser = reqparse.RequestParser()
-    parser.add_argument('sepal_length', type=str, required=True,
-                        help='sepal_length')
-    parser.add_argument('sepal_width', type=str, required=True,
-                        help='sepal_width')
-    parser.add_argument('petal_length', type=str, required=True,
-                        help='petal_length')
-    parser.add_argument('petal_width', type=str, required=True,
-                        help='petal_width')
+#    parser = reqparse.RequestParser()
+#    parser.add_argument('sepal_length', type=str, required=True,
+#                        help='sepal_length')
+#    parser.add_argument('sepal_width', type=str, required=True,
+#                        help='sepal_width')
+#    parser.add_argument('petal_length', type=str, required=True,
+#                        help='petal_length')
+#    parser.add_argument('petal_width', type=str, required=True,
+#                        help='petal_width')
 
-    def get(self):
-        args = IrisPredict.parser.parse_args()
-        float_features = [
-            float(args.get("sepal_length")),
-            float(args.get("sepal_width")),
-            float(args.get("petal_length")),
-            float(args.get("petal_width"))
-        ]
+#    def get(self):
+#        args = IrisPredict.parser.parse_args()
+#        float_features = [
+#            float(args.get("sepal_length")),
+#            float(args.get("sepal_width")),
+#            float(args.get("petal_length")),
+#            float(args.get("petal_width"))
+#        ]
 
-        print(float_features)
-        features = [np.array(float_features)]
-        prediction = model.predict(features)
-        return {'prediction_text':  "The flower species is {}".format(prediction)}
+#        print(float_features)
+#        features = [np.array(float_features)]
+#        prediction = model.predict(features)
+#        return {'prediction_text':  "The flower species is {}".format(prediction)}
 
 
-api.add_resource(All_Movies, '/')
-api.add_resource(Movies_List, '/<string:movie>')
-api.add_resource(IrisPredict, '/iris')
+api.add_resource(All_Candidate, '/candidate',)
+api.add_resource(Candidate_List, '/candidate/<string:name>')
+api.add_resource(All_Classroomsubjectclass, '/classroomsubjectclass')
+api.add_resource(Classroomsubjectclass_List, '/classroomsubjectclass/<string:name>')
+
+#api.add_resource(IrisPredict, '/iris')
 
 if __name__ == '__main__':
     # Run the applications
--- base.py
+++ base.py
@@ -6,36 +6,83 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()  # Creating database class
 
 
-class Movies(db.Model):
+classroomsubjectclasscandidate = db.Table('classroomsubjectclasscandidate',
+    db.Column('candidate_id',db.Integer,db.ForeignKey('candidate.candidate_id'),primary_key=True),
+    db.Column('classroom_subject_class_id',db.Integer,db.ForeignKey('classroomsubjectclass.classroom_subject_class_id'),primary_key=True),
+    db.Column('is_present',db.Boolean, unique=False, default=True)
+)
+
+class Candidate(db.Model):
 
     # Creating field/columns of the database as class variables
-    id = db.Column(db.Integer, primary_key=True)
+    candidate_id = db.Column(db.Integer, primary_key=True)
 
-    title = db.Column(db.String(30), unique=True, nullable=False)
+    name = db.Column(db.String(30), unique=True, nullable=False)
 
-    director = db.Column(db.String(30), unique=False, nullable=False)
+    keypoints = db.Column(db.String(30), unique=False, nullable=False)
 
-    genre = db.Column(db.String(30), unique=False, nullable=False)
+    descriptors = db.Column(db.String(30), unique=False, nullable=False)
 
-    collection = db.Column(db.Integer, unique=False, nullable=False)
+ 
 
-    def __init__(self, title, director, genre, collection):
-        self.title = title
-        self.director = director
-        self.genre = genre
-        self.collection = collection
+    def __init__(self, name, keypoints, descriptors):
+        self.name = name
+        self.keypoints = keypoints
+        self.descriptors = descriptors
 
     # Method to show data as dictionary object
     def json(self):
-        return {'Title': self.title,
-                'Director': self.director,
-                'Genre': self.genre,
-                'Collection': self.collection}
+        return {'name': self.name,
+                'keypoints': self.keypoints,
+                'descriptors': self.descriptors}
 
-    # Method to find the query movie is existing or not
+    # Method to find the query element is existing or not
     @classmethod
-    def find_by_title(cls, title):
-        return cls.query.filter_by(title=title).first()
+    def find_by_id(cls, id):
+        return cls.query.filter_by(candidate_id=id).first()
+
+    # Method to find the query element is existing or not
+    @classmethod
+    def find_by_name(cls, name):
+        return cls.query.filter_by(name=name).first()
+
+    # Method to save data to database
+    def save_to(self):
+        db.session.add(self)
+        db.session.commit()
+
+    # Method to delete data from database
+    def delete_(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class Classroomsubjectclass (db.Model):
+
+    # Creating field/columns of the database as class variables
+    classroom_subject_class_id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String(30), unique=True, nullable=False)
+    classroomsubjectclasscandidates = db.relationship('Candidate', secondary=classroomsubjectclasscandidate, lazy='dynamic',
+        backref=db.backref('classroomsubjectclass', lazy=True))
+
+ 
+
+    def __init__(self, name):
+        self.name = name
+
+    # Method to show data as dictionary object
+    def json(self):
+        return {'name': self.name}
+
+    # Method to find the query element is existing or not
+    @classmethod
+    def find_by_id(cls, id):
+        return cls.query.filter_by(candidate_id=id).first()
+
+    # Method to find the query element is existing or not
+    @classmethod
+    def find_by_name(cls, name):
+        return cls.query.filter_by(name=name).first()
 
     # Method to save data to database
     def save_to(self):
